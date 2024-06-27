c60bf13c619a6ff75a50dfa9d3910ad21ba020b6
kojix2 <2xijok@gmail.com>
2024-06-28 06:59:56 +0900

    Add comments to example

--- examples/cow.cr
+++ examples/cow.cr
@@ -1,7 +1,16 @@
 require "../src/cowsay"
 
+# This is a simple example of how to use the Cowsay module
+# as a library in your own code.
+
+# The Cowsay module provides a class method called say
+puts Cowsay.say("Hello, I am a cow")
+puts Cowsay.think(
+  "Hello, I am a cow",
+  character: "cow",
+  mode: "borg",
+)
+
+# You can also create a new cow object and call the say method on it
 cow = Cowsay::Cow.new
-
 puts cow.say("Hello, I am a cow")
-
-puts Cowsay.think("Hello, I am tux", type: "tux")
